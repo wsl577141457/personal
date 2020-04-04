6a5e3f8b70968ce3d04b720a26bd28678e0d31fe
wsl577141457 <577141457@qq.com>
2020-04-04 17:15:48 +0800

    第二次提交hibernate练习

---
+++ HibernateLX/src/main/java/com/longge/domain/User.java
@@ -0,0 +1,50 @@
+package com.longge.domain;
+
+public class User {
+    private Integer id;
+    private String code;
+    private String password;
+    private String name;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", code='" + code + '\'' +
+                ", password='" + password + '\'' +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
